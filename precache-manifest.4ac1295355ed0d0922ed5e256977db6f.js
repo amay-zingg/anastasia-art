62a5d160104784e41120188376f78e4bef9f60de
amay-zingg <anastasiamay29@gmail.com>
2020-09-25 03:15:43 -0400

    Updates

---
+++ precache-manifest.4ac1295355ed0d0922ed5e256977db6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3ffcc3c1ed68d75a3809a74e2d30ff",
+    "url": "/anastasia-art/index.html"
+  },
+  {
+    "revision": "5c5d1f7be11ebf15d6ba",
+    "url": "/anastasia-art/static/css/main.731505bb.chunk.css"
+  },
+  {
+    "revision": "a9ccf986cc0cad774e0a",
+    "url": "/anastasia-art/static/js/2.f0473b49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anastasia-art/static/js/2.f0473b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5d1f7be11ebf15d6ba",
+    "url": "/anastasia-art/static/js/main.7cd1616b.chunk.js"
+  },
+  {
+    "revision": "7f709618b08df1ee197a",
+    "url": "/anastasia-art/static/js/runtime-main.c86450f3.js"
+  },
+  {
+    "revision": "a2819b151ece114ab02d4b22e4a0d963",
+    "url": "/anastasia-art/static/media/Hero.a2819b15.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/anastasia-art/precache-manifest.4ac1295355ed0d0922ed5e256977db6f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/anastasia-art/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
