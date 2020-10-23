f44ab66dc7081f092f4570a8a8766571f0c8c3e5
amay-zingg <anastasiamay29@gmail.com>
2020-10-23 03:54:41 -0400

    Added some more paintings

--- src/App.js
+++ src/App.js
@@ -312,7 +312,52 @@ const caricatureArt = [
         title: '2015 Zoinks!',
         description: 'Nathan & Bella Drawn in "Scooby Doo" Style'
     },
-     {
+    {
+        image: 'https://www.anastasiamay.ca/assets/caricatureWatermark/natesDad.jpg',
+        title: '2015 James',
+        description: '2015 Nathan\'s Dad James Drawn in "Pink Panther" Style'
+    },
+    {
+        image: 'https://www.anastasiamay.ca/assets/caricatureWatermark/lizPokemon.jpg',
+        title: '2015 Elizabeth',
+        description: '2015 Elizabeth Drawn in "Pokemon" Style'
+    },
+    {
+        image: 'https://www.anastasiamay.ca/assets/caricatureWatermark/jamesDrSuess.jpg',
+        title: '2015 James',
+        description: '2015 JAmes Drawn in "Dr. Seuss" Style'
+    },
+    {
+        image: 'https://www.anastasiamay.ca/assets/caricatureWatermark/danoAndLarissa.jpg',
+        title: '2015 Dano & Larissa',
+        description: '2015 Dano & Larissa Drawn in "Popeye" Style'
+    },
+    {
+        image: 'https://www.anastasiamay.ca/assets/caricatureWatermark/gregKAdventureTime.jpg',
+        title: '2015 Greg K',
+        description: '2015 Greg K Drawn in "Adventure Time" Style'
+    },
+    {
+        image: 'https://www.anastasiamay.ca/assets/caricatureWatermark/kaylaAndAna.jpg',
+        title: '2013 Calvin & Hobbes',
+        description: '2013 Kayla and Ana Drawn in "Calvin & Hobbes" Style'
+    },
+    {
+        image: 'https://www.anastasiamay.ca/assets/caricatureWatermark/reneeWallace.jpg',
+        title: '2015 Renee',
+        description: '2015 Renee drawn in "Wallace & Grommit" Style'
+    },
+    {
+        image: 'https://www.anastasiamay.ca/assets/caricatureWatermark/lauraDesiree.jpg',
+        title: '2017 Laura',
+        description: '2017 Laura Desiree Drawn in my Style'
+    },
+    {
+        image: 'https://www.anastasiamay.ca/assets/caricatureWatermark/pawsitivelyPoop.jpg',
+        title: '2017 Pawsitively Poop',
+        description: '2017 Pawsitively Poop Coffee Beans'
+    },
+    {
         image: 'https://www.anastasiamay.ca/assets/caricatureWatermark/.jpg',
         title: '',
         description: ''
