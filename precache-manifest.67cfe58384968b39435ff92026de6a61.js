0766567ad5e02d239f238cf5a25966f9585fdbf7
amay-zingg <anastasiamay29@gmail.com>
2021-02-23 15:38:22 -0500

    Updates

--- precache-manifest.67cfe58384968b39435ff92026de6a61.js
+++ precache-manifest.67cfe58384968b39435ff92026de6a61.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "00a1341c4d13d37cd95079fa510dbc84",
+    "revision": "7d4c0b57e7089f19199b9b5be98778be",
     "url": "/anastasia-art/index.html"
   },
   {
-    "revision": "3174a9cc40a33e6ed345",
+    "revision": "5279c259ec94d6b249eb",
     "url": "/anastasia-art/static/css/main.93b7de8e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/anastasia-art/static/js/2.7155a536.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3174a9cc40a33e6ed345",
-    "url": "/anastasia-art/static/js/main.04770e33.chunk.js"
+    "revision": "5279c259ec94d6b249eb",
+    "url": "/anastasia-art/static/js/main.6a4daebd.chunk.js"
   },
   {
     "revision": "7f709618b08df1ee197a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/anastasia-art/precache-manifest.d4728f9b129477d9dd37bc06f0161c28.js"
+  "/anastasia-art/precache-manifest.67cfe58384968b39435ff92026de6a61.js"
 );
 
 self.addEventListener('message', (event) => {
